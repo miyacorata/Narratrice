712802245d10d6f2ab8e18ffa972d972d2dfe10a
K Miyano <miyano@miyacorata.net>
2022-01-27 01:38:34 +0900

    First commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import QueryBox from './components/QueryBox.vue';
+</script>
+
+<template>
+  <header>
+    <h1>Narratrice</h1>
+    <div>assaultlily-rdf simple frontend</div>
+  </header>
+  <QueryBox/>
+  <div>
+    ここに結果セットを吐き出すやつをつくるよ
+  </div>
+</template>
+
+<style>
+body {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin: 0;
+}
+
+header {
+  display: flex;
+  align-items: baseline;
+  background: linear-gradient(#94D7FF, skyblue);
+  padding: 3px 10px;
+  font-size: 17px;
+}
+header > h1 {
+  margin: 0 10px 0 0;
+  font-size: 20px;
+}
+</style>
---
+++ src/components/QueryBox.vue
@@ -0,0 +1,44 @@
+<template>
+  <textarea id="editor"></textarea>
+</template>
+
+<script>
+import CodeMirror from 'codemirror'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/addon/edit/matchbrackets'
+import 'codemirror/mode/sparql/sparql'
+
+const defaultCode = 
+`PREFIX lily: <https://lily.fvhp.net/rdf/IRIs/lily_schema.ttl#>
+PREFIX lilyrdf: <https://lily.fvhp.net/rdf/RDFs/detail/>
+PREFIX schema: <http://schema.org/>
+PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+
+`
+
+export default {
+  components: {
+    CodeMirror,
+  },
+  mounted () {
+    const editor = document.querySelector('#editor')
+    this.editor = CodeMirror.fromTextArea(editor, {
+      lineNumbers: true,
+      mode: 'sparql',
+    })
+    this.editor.setValue(defaultCode)
+  }
+}
+</script>
+
+<style>
+.CodeMirror {
+  border-bottom: solid 1px black !important;
+  height: 100px;
+  transition: height ease .3s;
+}
+.CodeMirror:hover{
+  height: 80vh;
+}
+</style>
